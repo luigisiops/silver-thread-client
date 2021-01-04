13bbb0d6d2f52e89caa43d433223d71222162d5e
S-Philp <shannel.philp@yahoo.com>
2021-01-04 15:26:29 -0600

    Need to change branches

--- src/login/ui/login.jsx
+++ src/login/ui/login.jsx
@@ -6,7 +6,23 @@ import { NavLink, Redirect } from "react-router-dom"
 import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
-import tileData from './tileData';
+// import tileData from './tileData';
+
+import ST_1 from '../images/ST_1.png';
+import ST_2 from '../images/ST_2.jpg';
+import ST_3 from '../images/ST_3.jpg';
+import ST_4 from '../images/ST_4.jpg';
+import ST_5 from '../images/ST_5.jpg';
+import ST_6 from '../images/ST_6.jpg';
+import ST_7 from '../images/ST_7.jpg';
+import ST_8 from '../images/ST_8.jpg';
+import ST_9 from '../images/ST_9.jpg';
+import ST_10 from '../images/ST_10.jpg';
+import ST_11 from '../images/ST_11.jpg';
+import ST_12 from '../images/ST_12.jpg';
+import ST_13 from '../images/ST_13.jpg';
+import ST_14 from '../images/ST_14.jpg';
+
 
 
 import "./login.css"
@@ -20,33 +36,90 @@ const useStyles = makeStyles((theme) => ({
       display: 'flex',
       flexWrap: 'wrap',
       justifyContent: 'space-around',
+      alignItems: 'stretch',
       overflow: 'hidden',
       backgroundColor: theme.palette.background.paper,
     },
     gridList: {
-      width: 500,
-      height: 450,
+      width: 800,
+    //   height: 900,
     },
   }));
-  
-  /**
-   * The example data is structured as follows:
-   *
-   * import image from 'path/to/image.jpg';
-   * [etc...]
-   *
-   * const tileData = [
-   *   {
-   *     img: image,
-   *     title: 'Image',
-   *     author: 'author',
-   *     cols: 2,
-   *   },
-   *   {
-   *     [etc...]
-   *   },
-   * ];
-   */
+
+const tileData = [
+   {
+     img: ST_3,
+     title: 'Image1',
+     author: 'SilverThreadDesigns',
+     cols: 1,
+   },
+   {
+    img: ST_4,
+    title: 'Image4',
+    author: 'SilverThreadDesigns',
+    cols: 1,
+   },
+   {
+    img: ST_5,
+    title: 'Image3',
+    author: 'SilverThreadDesigns',
+    cols: 1,
+   },
+   {
+    img: ST_6,
+    title: 'Image3',
+    author: 'SilverThreadDesigns',
+    cols: 1,
+   },
+   {
+    img: ST_7,
+    title: 'Image2',
+    author: 'SilverThreadDesigns',
+    cols: 1,
+   },
+   {
+    img: ST_8,
+    title: 'Image3',
+    author: 'SilverThreadDesigns',
+    cols: 1,
+   },
+   {
+    img: ST_9,
+    title: 'Image4',
+    author: 'SilverThreadDesigns',
+    cols: 1,
+   },
+   {
+    img: ST_11,
+    title: 'Image4',
+    author: 'SilverThreadDesigns',
+    cols: 1,
+   },
+   {
+    img: ST_12,
+    title: 'Image4',
+    author: 'SilverThreadDesigns',
+    cols: 1,
+   },
+   {
+    img: ST_13,
+    title: 'Image4',
+    author: 'SilverThreadDesigns',
+    cols: 1,
+   },
+   {
+    img: ST_14,
+    title: 'Image4',
+    author: 'SilverThreadDesigns',
+    cols: 1,
+   },
+   {
+    img: ST_10,
+    title: 'Image4',
+    author: 'SilverThreadDesigns',
+    cols: 1,
+   },
+];
 
 const Login = ({onLogin, getLoggedUser, user}) => {
 
@@ -69,10 +142,19 @@ const Login = ({onLogin, getLoggedUser, user}) => {
 
     return (
         <div className="login-component">
-            <div className="left-container">
+            <div className={classes.root} style={{ background: 'transparent'}} elevation={0}>
+                <GridList cellHeight={290} cellWidth={300} className={classes.gridList} cols={4}>
+                    {tileData.map((tile) => (
+                    <GridListTile key={tile.img} cols={tile.cols || 1} rows={tile.rows || 1}>
+                        <img src={tile.img} alt={tile.title} />
+                    </GridListTile>
+                    ))}
+                </GridList>
+            </div>
+            {/* <div className="left-container">
                 <h2 className="blurb">A Data Managment User Interface for Silver Thread Designs</h2>
                 <h2 className="blurb">Handle your business logic via linked tables and graphical interfaces</h2>
-            </div>
+            </div> */}
 
             <div className="login-container">
                 <h2 className = "login-blurb">See Your Business Data Now</h2>
