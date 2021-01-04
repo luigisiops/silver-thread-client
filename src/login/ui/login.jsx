81539c15aecd32ef563506cbe8b7601b26d0f699
S-Philp <shannel.philp@yahoo.com>
2021-01-04 13:35:19 -0600

    Started PictureGrid

--- src/login/ui/login.jsx
+++ src/login/ui/login.jsx
@@ -3,13 +3,50 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { connect } from "react-redux"
 import { NavLink, Redirect } from "react-router-dom"
-
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import tileData from './tileData';
 
 
 import "./login.css"
 import { onLogin } from '../frameworks/actions';
 import { UserLogin } from '../use-cases/UserLogin'
 import { CheckUser } from '../use-cases/CheckUser'
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      overflow: 'hidden',
+      backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+      width: 500,
+      height: 450,
+    },
+  }));
+  
+  /**
+   * The example data is structured as follows:
+   *
+   * import image from 'path/to/image.jpg';
+   * [etc...]
+   *
+   * const tileData = [
+   *   {
+   *     img: image,
+   *     title: 'Image',
+   *     author: 'author',
+   *     cols: 2,
+   *   },
+   *   {
+   *     [etc...]
+   *   },
+   * ];
+   */
 
 const Login = ({onLogin, getLoggedUser, user}) => {
 
@@ -26,6 +63,10 @@ const Login = ({onLogin, getLoggedUser, user}) => {
         getLoggedUser()
     },[])
     console.log(fields)
+
+    const classes = useStyles();
+
+
     return (
         <div className="login-component">
             <div className="left-container">
