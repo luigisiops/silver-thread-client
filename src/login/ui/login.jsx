7a2175a7c0670e45a1e275ce3277d3907ab9f47e
S-Philp <shannel.philp@yahoo.com>
2021-01-05 18:21:44 -0600

    Added photo cards to login page

--- src/login/ui/login.jsx
+++ src/login/ui/login.jsx
@@ -4,11 +4,11 @@ import Button from '@material-ui/core/Button';
 import { connect } from "react-redux"
 import { NavLink, Redirect } from "react-router-dom"
 import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import tileData from './tileData';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import Toolbar from '@material-ui/core/Toolbar';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import { Container, AppBar, Grid, Card, CardMedia } from '@material-ui/core';
 
 
 import "./login.css"
@@ -16,22 +16,57 @@ import { onLogin } from '../frameworks/actions';
 import { UserLogin } from '../use-cases/UserLogin'
 import { CheckUser } from '../use-cases/CheckUser'
 
+import logo from '../images/logo.png'
+import ST_3 from '../images/ST_3.jpg';
+import ST_4 from '../images/ST_4.jpg';
+import ST_5 from '../images/ST_5.jpg';
+import ST_6 from '../images/ST_6.jpg';
+import ST_7 from '../images/ST_7.jpg';
+import ST_8 from '../images/ST_8.jpg';
+import ST_9 from '../images/ST_9.jpg';
+import ST_10 from '../images/ST_10.jpg';
+import ST_11 from '../images/ST_11.jpg';
+import ST_12 from '../images/ST_12.jpg';
+import ST_13 from '../images/ST_13.jpg';
+import ST_14 from '../images/ST_14.jpg';
+
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      justifyContent: 'space-around',
-      alignItems: 'stretch',
-      overflow: 'hidden',
-      backgroundColor: theme.palette.background.paper,
-    },
-    gridList: {
-      width: 1500,
-    },
-    pita: {
-        minWidth: 275,
-    }
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+      },
+      appBar: {
+        borderRadius: 15,
+        marginTop: '10px',
+        marginBottom: '30px',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        color: '#5f5f5f',
+      },
+      media: {
+        height: 300,
+      },
+      card: {
+        maxHeight: 1000,
+        maxWidth: 400,
+      },
+      image: {
+          borderRadius: 16,
+      },
+      big: {
+          backgroundColor: '#4db6ac',
+      }
   }));
 
 
@@ -52,41 +87,166 @@ const Login = ({onLogin, getLoggedUser, user}) => {
     console.log(fields)
 
     const classes = useStyles();
-
+    
+    const [open, setOpen] = React.useState(false);
+  
+    const handleOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
 
     return (
-        <div className="login-component">
-            <div className={classes.root} style={{ background: 'transparent'}} elevation={0}>
-                <GridList cellHeight={350} cellWidth={400} className={classes.gridList} cols={4}>
-                    {tileData.map((tile) => (
-                    <GridListTile key={tile.img} cols={tile.cols || 1} rows={tile.rows || 1}>
-                        <img src={tile.img} alt={tile.title} />
-                    </GridListTile>
-                    ))}
-                </GridList>
-            </div>
-            {/* <div className="left-container">
-                <h2 className="blurb">A Data Managment User Interface for Silver Thread Designs</h2>
-                <h2 className="blurb">Handle your business logic via linked tables and graphical interfaces</h2>
-            </div> */}
-
-            {/* <Card className={classes.pita}>
-            <CardContent> */}
-            <div className="login-container">
-                <h2 className = "login-blurb">See Your Business Data Now</h2>
-                <div className="login-box">
-                    <div className="login-input"><TextField id="outlined-basic" label="Username" variant="outlined" name = "username" onChange = {setField}/></div>
-                    <div className="login-input"><TextField id="outlined-basic" label="Password" variant="outlined" type = "password" name = "password" onChange = {setField}/></div>
-                    <div>Not a user? <NavLink to="/register"> Register</NavLink> and get permission from a moderator!</div>
-                    <div className = "button-container">
-                       <Button onClick = {()=>onLogin(fields)}className ="login-button"variant="outlined">Login</Button>
-                       {Object.keys(user).length>0 ? <Redirect to="/"/> : <div></div>}
-                    </div>
-                </div>
-            </div>
-            {/* </CardContent>
-            </Card> */}
-        </div>
+            <Container className={classes.big} maxWidth="xlg" flexDirection="column">
+                <AppBar position="static" className={classes.appBar}>
+                    <Toolbar>
+                    <img className={classes.image} src={logo} alt="logo" height="60" />
+                    <Button color="inherit" onClick={handleOpen}>Login</Button>
+                    <Button color="inherit" onClick={handleClose}>Logout</Button>
+                    </Toolbar>
+                        <Modal
+                            aria-labelledby="transition-modal-title"
+                            aria-describedby="transition-modal-description"
+                            className={classes.modal}
+                            open={open}
+                            onClose={handleClose}
+                            closeAfterTransition
+                            BackdropComponent={Backdrop}
+                            BackdropProps={{
+                                timeout: 500,
+                            }}
+                            >
+                            <Fade in={open}>
+                                <div className={classes.paper}>
+                                    <div className="login-container">
+                                        <h2 className = "login-blurb">Silverthread Designs</h2>
+                                        <div className="login-box">
+                                            <div className="login-input"><TextField id="outlined-basic" label="Username" variant="outlined" name = "username" onChange = {setField}/></div>
+                                            <div className="login-input"><TextField id="outlined-basic" label="Password" variant="outlined" type = "password" name = "password" onChange = {setField}/></div>
+                                            <div>Not a user? <NavLink to="/register"> Register</NavLink> and get permission from a moderator!</div>
+                                            <div className = "button-container">
+                                            <Button onClick = {()=>onLogin(fields)}className ="login-button"variant="outlined">Login</Button>
+                                            {Object.keys(user).length>0 ? <Redirect to="/"/> : <div></div>}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Fade>
+                        </Modal>
+                    </AppBar>
+                 <Grid container flexGrow={1} justify="space-between" alignItems="center" spacing={3} justify="center" flexDirection="row">
+                     <Grid item xs={6} sm={3}>
+                            <Card className={classes.card}>
+                                <CardMedia
+                                className={classes.media}
+                                image={ST_3}
+                                title="Drop Earrings"
+                                />
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6} sm={3}>
+                            <Card className={classes.card}>
+                                <CardMedia
+                                className={classes.media}
+                                image={ST_4}
+                                title="Drop Earrings"
+                                />
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6} sm={3}>
+                            <Card className={classes.card}>
+                                <CardMedia
+                                className={classes.media}
+                                image={ST_5}
+                                title="Drop Ring"
+                                />
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6} sm={3}>
+                            <Card className={classes.card}>
+                                <CardMedia
+                                className={classes.media}
+                                image={ST_6}
+                                title="Drop Ring"
+                                />
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6} sm={3}>
+                            <Card className={classes.card}>
+                                <CardMedia
+                                className={classes.media}
+                                image={ST_7}
+                                title="Drop Necklace"
+                                />
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6} sm={3}>
+                            <Card className={classes.card}>
+                                <CardMedia
+                                className={classes.media}
+                                image={ST_8}
+                                title="Drop Bracelet"
+                                />
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6} sm={3}>
+                            <Card className={classes.card}>
+                                <CardMedia
+                                className={classes.media}
+                                image={ST_9}
+                                title="Mezuzah"
+                                />
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6} sm={3}>
+                            <Card className={classes.card}>
+                                <CardMedia
+                                className={classes.media}
+                                image={ST_10}
+                                title="Drop Earrings"
+                                />
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6} sm={3}>
+                            <Card className={classes.card}>
+                                <CardMedia
+                                className={classes.media}
+                                image={ST_11}
+                                title="Drop Earrings"
+                                />
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6} sm={3}>
+                            <Card className={classes.card}>
+                                <CardMedia
+                                className={classes.media}
+                                image={ST_12}
+                                title="Drop Necklace"
+                                />
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6} sm={3}>
+                            <Card className={classes.card}>
+                                <CardMedia
+                                className={classes.media}
+                                image={ST_13}
+                                title="Mezuzah"
+                                />
+                            </Card>
+                        </Grid>
+                        <Grid item xs={6} sm={3}>
+                            <Card className={classes.card}>
+                                <CardMedia
+                                className={classes.media}
+                                image={ST_14}
+                                title="Reticulated Silver Earrings"
+                                />
+                            </Card>
+                        </Grid>
+                 </Grid>
+        </Container>
     )
 }
 
