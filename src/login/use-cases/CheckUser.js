69d18ffeeafe46f99e50f7c2f6c065034f197c5e
Lsiopongco <58718096+luigisiops@users.noreply.github.com>
2021-01-07 10:24:27 -0500

    deleted extra charts

--- src/login/use-cases/CheckUser.js
+++ src/login/use-cases/CheckUser.js
@@ -13,7 +13,6 @@ export const CheckUser = (dispatch) => async(
       })
       //need to await on parsing response to javascript objects from json
       const success = await response.json()
-      console.log(JSON.stringify(success))
 
       //pass into our action as a payload and we dispatch it
       if (!success.user){
